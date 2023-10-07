d423cd4dbcdaa02086256edd7a31f1fb765de16c
Mr.Chen <2490197735@qq.com>
2023-10-07 18:34:56 +0800

    改进

--- src/main/java/com/struggle/oj/service/impl/QuestionSubmitServiceImpl.java
+++ src/main/java/com/struggle/oj/service/impl/QuestionSubmitServiceImpl.java
@@ -3,6 +3,7 @@ package com.struggle.oj.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.struggle.oj.judge.JudgeService;
 import com.struggle.oj.model.dto.questionsubmit.QuestionSubmitQueryRequest;
 import com.struggle.oj.model.entity.QuestionSubmit;
 import com.struggle.oj.model.enums.QuestionSubmitLanguageEnum;
@@ -22,10 +23,12 @@ import com.struggle.oj.utils.SqlUtils;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 /**
@@ -39,6 +42,10 @@ public class QuestionSubmitServiceImpl extends ServiceImpl<QuestionSubmitMapper,
 
     @Resource
     private UserService userService;
+
+    @Resource
+    @Lazy
+    private JudgeService judgeService;
 
     /**
      * 提交题目
@@ -76,7 +83,12 @@ public class QuestionSubmitServiceImpl extends ServiceImpl<QuestionSubmitMapper,
         if (!save) {
             throw new BusinessException(ErrorCode.FORBIDDEN_ERROR, "数据插入失败");
         }
-        return questionSubmit.getId();
+        Long questionSubmitId = questionSubmit.getId();
+        //执行判题服务
+        CompletableFuture.runAsync(() ->{
+            judgeService.doJudge(questionSubmitId);
+        });
+        return questionSubmitId;
     }
 
     /**
